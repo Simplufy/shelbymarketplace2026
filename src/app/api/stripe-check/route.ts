264e6ea24465984a453caf0e97d61d475826f2e6
Simplufy Team <team@simplufy.com>
2026-04-13 18:18:55 -0400

    Fix to GET

--- src/app/api/stripe-check/route.ts
+++ src/app/api/stripe-check/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import Stripe from 'stripe';
 
-export async function POST() {
+export async function GET() {
   const key = process.env.STRIPE_SECRET_KEY;
   
   if (!key) {
